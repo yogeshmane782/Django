5769830baaad64f762b811c79c25c4ab2524c670
Yogesh Mane <yogeshmane41799@gmail.com>
2025-05-20 12:28:20 +0530

    push commit all

--- project12/app1/views.py
+++ project12/app1/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method=='POST':
+        form=CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=CustomUserCreationForm()
+    return render(request,"app1/register.html",{'form':form})
+def login_view(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password1=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return render(request,"app1/login.html",{'error':'Invalid Creditials'})
+    return render(request,"app1/login.html")
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+@login_required
+def home(request):
+    return render(request,'app1/home.html')
--- project12/project12/settings.py
+++ project12/project12/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1'
 ]
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'project12.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
